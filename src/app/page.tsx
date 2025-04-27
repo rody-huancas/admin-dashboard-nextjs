567e732ec15cfa68b165760c6d68ba4528b4b7ec
Rody Huancas <rodyhuancas.04@gmail.com>
2025-04-26 20:12:01 -0500

    feat: add new chart components and integrate them into the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
