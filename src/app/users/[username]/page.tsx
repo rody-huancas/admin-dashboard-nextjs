6f789f5e040c0fae0f8ab34fb9ad6d541db8613a
Rody Huancas <rodyhuancas.04@gmail.com>
2025-04-26 22:29:48 -0500

    feat: add Radix UI components (HoverCard, Progress) and integrate into user page; update styles and structure

---
+++ src/app/users/[username]/page.tsx
@@ -0,0 +1,165 @@
+import CardList from "@/components/CardList";
+import { Badge } from "@/components/ui/badge";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+
+const SingleUserPage = () => {
+  return (
+    <div className="space-y-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/users">Usuarios</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Rody</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      {/* Container */}
+      <div className="flex flex-col xl:flex-row gap-8">
+        {/* LEFT */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* USER BADGES CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">Insignias de Usuario</h1>
+
+            <div className="flex gap-4 mt-4">
+              <HoverCard>
+                <HoverCardTrigger>
+                  <BadgeCheck
+                    size={36}
+                    className="rounded-full bg-blue-500/30 border border-blue-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verificar Usuario</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Este usuario ha sido verificado por el administrador
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Citrus
+                    size={36}
+                    className="rounded-full bg-green-500/30 border border-green-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verificar Usuario</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Este usuario ha sido verificado por el administrador
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Candy
+                    size={36}
+                    className="rounded-full bg-yellow-500/30 border border-yellow-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verificar Usuario</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Este usuario ha sido verificado por el administrador
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Shield
+                    size={36}
+                    className="rounded-full bg-orange-500/30 border border-orange-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verificar Usuario</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Este usuario ha sido verificado por el administrador
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+          </div>
+          {/* INFORMATION CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">Información del Usuario</h1>
+
+            <div className="space-y-4 mt-4">
+              <div className="flex flex-col gap-2 mb-8">
+                <p className="text-sm text-muted-foreground">
+                  Perfil Completado
+                </p>
+
+                <Progress value={66} />
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Username:</span>
+                <span>rodyhuancas</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">E-mail:</span>
+                <span>rodyhuancas@correo.com</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Celular:</span>
+                <span>+51 999 999 999</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Ubicación:</span>
+                <span>Chiclayo, Perú</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bold">Rol:</span>
+                <Badge>Administrador</Badge>
+              </div>
+            </div>
+
+            <p className="text-sm text-muted-foreground mt-4">
+              Se unió el 2025.01.01
+            </p>
+          </div>
+          {/* CARD LIST CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className="w-full xl:w-2/3 space-y-6">
+          {/* USER CARD CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
+          {/* CHART CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
